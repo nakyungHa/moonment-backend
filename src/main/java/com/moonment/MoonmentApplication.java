a98e645ed426aa112a93e1b275da82c8da6cc0e4
nakyungHa <nkha2005@gmail.com>
2026-06-14 13:48:39 +0900

    init: create spring boot project [moonment]

---
+++ src/main/java/com/moonment/MoonmentApplication.java
@@ -0,0 +1,13 @@
+package com.moonment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonmentApplication.class, args);
+    }
+
+}
