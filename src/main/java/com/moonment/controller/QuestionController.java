284daeadbeddcde3f899b7cc2be64595e2a12822
nakyungHa <nkha@ewha.ac.kr>
2026-06-24 17:11:20 +0900

    feat: setup question api structure

--- src/main/java/com/moonment/controller/QuestionController.java
+++ src/main/java/com/moonment/controller/QuestionController.java
@@ -1,6 +1,11 @@
 package com.moonment.controller;
 
+import com.moonment.dto.question.QuestionResponse;
+import com.moonment.entity.Question;
+import com.moonment.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/questions")
 @RequiredArgsConstructor
 public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping("/{id}")
+    public QuestionResponse getQuestion(@PathVariable Integer id) {
+
+        Question question = questionService.getQuestion(id);
+
+        return new QuestionResponse(
+                question.getQuestionId(),
+                question.getQuestionType().getType(),
+                question.getContent()
+        );
+    }
 }
--- src/main/java/com/moonment/dto/question/QuestionResponse.java
+++ src/main/java/com/moonment/dto/question/QuestionResponse.java
@@ -1,4 +1,15 @@
 package com.moonment.dto.question;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class QuestionResponse {
+
+    private Integer questionId;
+
+    private String type;
+
+    private String content;
 }
